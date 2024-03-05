761b18234e633a266463a7b9acbb39485bf3533c
Sultan <sitexpressorg@gmail.com>
2024-03-05 21:09:54 +0300

    First one

---
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const ErrorPage = () => {
+    return (
+        <div>
+            <span>Error</span>
+        </div>
+    );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import App from "./app/App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
+import { ErrorPage } from "./components/ErrorPage/ErrorPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />
+    }
+    // {
+    //     path: "/search-result-page",
+    //     element: <SearchingResultPage />
+    // },
+    // {
+    //     path: "/book-details-page",
+    //     element: <BookDetails />
+    // }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
